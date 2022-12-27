27581325fe15a5155a8ef8505bca76571cf21a48
Murtadha <78985176+MurtadhaInit@users.noreply.github.com>
2022-12-27 21:07:42 +0000

    Install apprise instead of ntfy

--- config/zsh/aliases.zsh
+++ config/zsh/aliases.zsh
@@ -3,6 +3,7 @@ alias exa='exa -lah --git --group-directories-first --no-time'
 alias man=batman
 alias grep='grep --color'
 alias bbd='brew bundle dump --force --describe --file=~/.dotfiles/Homebrew/Brewfile'  # Update the Brewfile
+alias uninstall='$HOME/Scripts/utility_scripts/app-cleaner.sh'  # Remove apps
 
 # Using parameter expansion, apply the newline-separator option to the lowercase array version of 'path' and redirect the result to standard output using hereword (the default for which we've changed to bat).
 alias trail='<<<${(F)path}'
