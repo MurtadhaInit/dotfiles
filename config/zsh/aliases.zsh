e34f8ea11e0b2011e25ecbc30a6f2efeeb45781a
Murtadha <78985176+MurtadhaInit@users.noreply.github.com>
2023-02-19 09:05:01 +0000

    Obsidian backup script is now .sh not .zsh
    - Install Terraform
    - Install Postman
    - Install Downie

--- config/zsh/aliases.zsh
+++ config/zsh/aliases.zsh
@@ -4,7 +4,7 @@ alias man=batman
 alias grep='grep --color'
 alias bbd='brew bundle dump --force --describe --file=~/.dotfiles/Homebrew/Brewfile'  # Update the Brewfile
 alias uninstall='$HOME/Scripts/utility_scripts/app-cleaner.sh'  # Remove apps
-alias obsidian_backup='$HOME/Scripts/periodic_scripts/obsidian_sync.zsh'
+alias obsidian_backup='$HOME/Scripts/periodic_scripts/obsidian_sync.sh'
 
 # Using parameter expansion, apply the newline-separator option to the lowercase array version of 'path' and redirect the result to standard output using hereword (the default for which we've changed to bat).
 alias trail='<<<${(F)path}'
