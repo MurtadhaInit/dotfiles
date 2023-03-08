8904c4d70f0d5ef31829384b000ac6d951bde63c
Murtadha <78985176+MurtadhaInit@users.noreply.github.com>
2023-03-08 00:58:46 +0000

    Cleanup and changes
    - Add .warp workflows folder to dotfiles
    - Install Handbrake, remove and re-install rectangle through Homebrew, install VIA and Slack, try out SigmaOS
    - Remove the loading of goenv leftover, remove Anaconda initialisation which is not used
    - Remove p4v and edit .config file for git to not use it anymore (use VSCode merge editor)

--- config/zsh/tools.zsh
+++ config/zsh/tools.zsh
@@ -1,24 +1,6 @@
 # Load Homebrew and add its tools to $path
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
-# Load Anaconda
-# >>> conda initialize >>>
-# # # !! Contents within this block are managed by 'conda init' !!
-# __conda_setup="$('/Users/murtadha/.local/share/pyenv/versions/anaconda3-2022.05/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
-# if [ $? -eq 0 ]; then
-#     eval "$__conda_setup"
-# else
-#     if [ -f "/Users/murtadha/.local/share/pyenv/versions/anaconda3-2022.05/etc/profile.d/conda.sh" ]; then
-#         . "/Users/murtadha/.local/share/pyenv/versions/anaconda3-2022.05/etc/profile.d/conda.sh"
-#     else
-#         export PATH="/Users/murtadha/.local/share/pyenv/versions/anaconda3-2022.05/bin:$PATH"
-#     fi
-# fi
-# unset __conda_setup
-# <<< conda initialize <<<
-# The base environment is not activated by default
-# conda config --set auto_activate_base False
-
 # Load pyenv
 eval "$(pyenv init -)"
 # we want pyenv after Anaconda for the `conda` command to be
@@ -26,6 +8,3 @@ eval "$(pyenv init -)"
 
 # pyenv-virtualenv: enable auto-activation
 if which pyenv-virtualenv-init > /dev/null; then eval "$(pyenv virtualenv-init -)"; fi
-
-# Load goenv
-eval "$(goenv init -)"
