30f812eb305fd525f68f21f37bcdcc468dfaf007
Murtadha <78985176+MurtadhaInit@users.noreply.github.com>
2023-03-09 17:20:28 +0000

    Add two shell functions and remove Warp workflows
    - Removed Downie
    - Install Notion with Homebrew
    - Install yt-dlp
    - Add function chtsht and remove its equivalent from .warp/workflows
    - Add function uninstall and remove its alias equivalent
    - Add alias 'brew_outdated' and remove its equivalent warp workflow

--- config/zsh/aliases.zsh
+++ config/zsh/aliases.zsh
@@ -2,9 +2,10 @@ alias ls=exa
 alias exa='exa -lah --git --group-directories-first'
 alias man=batman
 alias grep='grep --color'
-alias bbd='brew bundle dump --force --describe --file=~/.dotfiles/Homebrew/Brewfile'  # Update the Brewfile
-alias uninstall='$HOME/Scripts/utility_scripts/app-cleaner.sh'  # Remove apps
+# Update the Brewfile
+alias bbd='brew bundle dump --force --describe --file=~/.dotfiles/Homebrew/Brewfile'
 alias obsidian_backup='$HOME/Scripts/periodic_scripts/obsidian_sync.sh'
+alias brew_outdated='brew update &>/dev/null && brew outdated'
 
 # Using parameter expansion, apply the newline-separator option to the lowercase array version of 'path' and redirect the result to standard output using hereword (the default for which we've changed to bat).
 alias trail='<<<${(F)path}'
--- config/zsh/functions.zsh
+++ config/zsh/functions.zsh
@@ -1,5 +1,15 @@
-# make a directory and then cd into it
-# TODO: load it lazily? autoload -Uz mkcd
+# Make a directory and then cd into it
 function mkcd() {
   mkdir -p "$@" && cd "$_";
+}
+
+# View a cheat sheet for any app or technology using cheat.sh
+# NOTE: use this or tldr
+function chtsht() {
+  curl cheat.sh/$@
+}
+
+# Remove apps
+function uninstall() {
+  $HOME/Scripts/utility_scripts/app-cleaner.sh $@
 }
