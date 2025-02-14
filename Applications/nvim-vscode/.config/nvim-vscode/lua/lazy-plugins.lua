45d1608e4427495591c1ed99037070bb232bf07c
Murtadha <78985176+MurtadhaInit@users.noreply.github.com>
2025-02-15 01:03:01 +0300

    nvim and nvim-vscode: use the mini surround plugin and bring everything up to date

--- Applications/nvim-vscode/.config/nvim-vscode/lua/lazy-plugins.lua
+++ Applications/nvim-vscode/.config/nvim-vscode/lua/lazy-plugins.lua
@@ -1,12 +1,9 @@
 require('lazy').setup({
   {
-    "kylechui/nvim-surround",
-    version = "*", -- Use for stability; omit to use `main` branch for the latest features
-    event = "VeryLazy",
-    config = function()
-        require("nvim-surround").setup({
-            -- Configuration here, or leave empty to use defaults
-        })
+    'echasnovski/mini.surround',
+    version = '*',
+    config = function ()
+      require('mini.surround').setup()
     end
   }
 })
