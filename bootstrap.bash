183a9389b668e08777cb5ec3c340d4e93273cc32
Murtadha <78985176+MurtadhaInit@users.noreply.github.com>
2025-08-07 18:56:14 +0300

    Revamped the bootstrap script to make it linux compatible and refactored it to be simpler

--- bootstrap.bash
+++ bootstrap.bash
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-# curl -sSfL https://raw.githubusercontent.com/MurtadhaInit/dotfiles/refs/heads/main/bootstrap.bash | bash
+# /bin/bash -c "$(curl -sSfL https://raw.githubusercontent.com/MurtadhaInit/dotfiles/refs/heads/main/bootstrap.bash)"
 
 # This script will:
-# 1. Install Git, Homebrew (on MacOS), and nu
-# 2. Clone the dotfiles repo
-# 3. Initiate the setup process
+# 1. Install Homebrew and then Git (with Homebrew)
+# 2. Clone the dotfiles repo to ~/.dotfiles
+# 3. Install Nushell with Homebrew
+# 4. Initiate the setup process with Nu scripts
 
 # check if a command exists
 function exists() {
@@ -17,66 +18,51 @@ function exists() {
 OS_TYPE=$(uname)
 case "$OS_TYPE" in
   ("Darwin")
-    # Install Homebrew without user interaction
-    # This will also install Xcode Command Line Tools if not present TODO: confirm so
-    if ! exists brew; then
-      echo "Installing Homebrew..."
-      NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-      eval "$(/opt/homebrew/bin/brew shellenv)"
-      brew analytics off
-    fi
-    
-    if ! exists git; then
-      echo "Installing Git..."
-      brew install git
-    fi
-    
-    if [ ! -d "$HOME/.dotfiles" ]; then
-      git clone --recursive https://github.com/MurtadhaInit/dotfiles.git "$HOME/.dotfiles"
-    else
-      echo "Dotfiles directory present ✅"
-    fi
-
-    if ! exists nu; then
-      echo "Installing Nushell..."
-      brew install nushell
-    fi
-
-    if [ "$#" -eq 0 ]; then
-        # No arguments provided
-        "$HOME"/.dotfiles/system-setup/setup.nu --skip
-        # "$HOME"/.dotfiles/system-setup/start.nu --skip-tasks ["8-all-apps"]
-    else
-        # Pass all arguments to the nushell script
-        "$HOME"/.dotfiles/system-setup/setup.nu "$@"
-    fi
+    brew_binary=/opt/homebrew/bin/brew
     ;;
   ("Linux")
-    # IMPORTANT: this assumes a Fedora system (with DNF)
-    # TODO: refactor to specifically run this block on Fedora-like distributions only
-    if ! exists git; then
-      echo "Installing Git..."
-      sudo dnf install git -y
-    fi
-
-    if [ ! -d "$HOME/.dotfiles" ]; then
-      git clone --recursive https://github.com/MurtadhaInit/dotfiles.git "$HOME/.dotfiles"
-    else
-      echo "Dotfiles directory present ✅"
-    fi
-
-    if ! exists ansible-playbook; then
-      echo "Installing Ansible..."
-      sudo dnf install ansible -y
-    fi
-
-    ansible-playbook --ask-become "$HOME/.dotfiles/system-setup/linux-setup/local.yml"
+    brew_binary=/home/linuxbrew/.linuxbrew/bin/brew
     ;;
   ("CYGWIN"* | "MINGW"* | "MSYS"*)
-    # Windows-specific actions
+    echo "⚠️ Windows not supported"
+    exit 1
     ;;
   (*)
-    echo "Unknown OS: $OS_TYPE"
+    echo "⚠️ Unknown OS: $OS_TYPE"
     exit 1
     ;;
 esac
+
+# This will presumably also install Xcode Command Line Tools (on macOS) if not present
+if ! exists brew; then
+  echo "🔄 Installing Homebrew..."
+  NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+eval "$($brew_binary shellenv)"
+brew analytics off
+
+if ! exists git; then
+  echo "🔄 Installing Git..."
+  brew install git
+fi
+
+if [ ! -d "$HOME/.dotfiles" ]; then
+  git clone --recursive https://github.com/MurtadhaInit/dotfiles.git "$HOME/.dotfiles"
+else
+  echo "✅ Dotfiles directory present"
+fi
+
+if ! exists nu; then
+  echo "🔄 Installing Nushell..."
+  brew install nushell
+fi
+
+if [ "$#" -eq 0 ]; then
+    # No arguments provided
+    "$HOME"/.dotfiles/system-setup/setup.nu --skip
+else
+    # Pass all arguments to the nushell script
+    "$HOME"/.dotfiles/system-setup/setup.nu "$@"
+fi
+
+# ansible-playbook --ask-become "$HOME/.dotfiles/system-setup/linux-setup/local.yml"
