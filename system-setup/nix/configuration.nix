17b5d37457131619a8ad4324a0fed28de18fe1fe
Murtadha <78985176+MurtadhaInit@users.noreply.github.com>
2025-08-16 19:56:56 +0300

    Fixed issues related to mounting my internal NTFS drives - enabled NTFS support on boot - changed the file system type for the drives form ntfs3 to ntfs-3g (don't know if that made the difference) - assigned a user and group for the drive permissions (for all files) which used to be root before - everything seems to be working fine now

--- system-setup/nix/configuration.nix
+++ system-setup/nix/configuration.nix
@@ -25,13 +25,26 @@
   # Automatically mount internal HDDs
   fileSystems."/run/media/murtadha/Files" = {
     device = "/dev/disk/by-uuid/4EF611C1F611A9EB";
-    fsType = "ntfs3";
+    fsType = "ntfs-3g";
+    options = [
+      "defaults"
+      "nofail"
+      "uid=1000"
+      "gid=100"
+    ];
   };
-
   fileSystems."/run/media/murtadha/New Volume" = {
     device = "/dev/disk/by-uuid/008A4B1F8A4B1098";
-    fsType = "ntfs3";
+    fsType = "ntfs-3g";
+    options = [
+      "defaults"
+      "nofail"
+      "uid=1000"
+      "gid=100"
+    ];
   };
+  boot.supportedFilesystems = [ "ntfs" ];
+  # services.udisks2.enable = true; # is that needed by any means?
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
