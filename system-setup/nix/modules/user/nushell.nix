f28e1645e6a1ac91269fe9711e8258cd8d822d6e
Murtadha <78985176+MurtadhaInit@users.noreply.github.com>
2025-09-02 18:28:00 +0300

    Sourced Nushell themes file with Nix (fetchFromGithub) and the extraConfig option. Worked flawlessly!

--- system-setup/nix/modules/user/nushell.nix
+++ system-setup/nix/modules/user/nushell.nix
@@ -1,10 +1,30 @@
-{ config, pkgs, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 
+let
+  # the catppuccin themes repo for Nushell
+  catppuccin-nushell = pkgs.fetchFromGitHub {
+    owner = "catppuccin";
+    repo = "nushell";
+    rev = "main";
+    hash = "sha256-tQ3Br6PaLBUNIXY56nDjPkthzvgEsNzOp2gHDkZVQo0=";
+  };
+
+  # the theme flavour file in the repo
+  mochaTheme = "${catppuccin-nushell}/themes/catppuccin_mocha.nu";
+in
 {
   programs.nushell = {
     enable = true;
     configFile.source = ../../../../Applications/nushell/.config/nushell/config.nu;
-  };
 
-  # install the themes
+    # source the theme file
+    extraConfig = ''
+      source ${mochaTheme}
+    '';
+  };
 }
