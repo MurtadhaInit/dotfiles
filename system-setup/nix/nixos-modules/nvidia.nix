75e14fbebad8f4474378e57d61e16a8b5b68a5d7
Murtadha <78985176+MurtadhaInit@users.noreply.github.com>
2026-06-13 19:41:02 +0300

    fix(nixos): nvidia drivers for my card has become legacy and need to be pinned

--- system-setup/nix/nixos-modules/nvidia.nix
+++ system-setup/nix/nixos-modules/nvidia.nix
@@ -31,6 +31,6 @@
     # accessible via `nvidia-settings`.
     nvidiaSettings = true;
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.legacy_580;
   };
 }
