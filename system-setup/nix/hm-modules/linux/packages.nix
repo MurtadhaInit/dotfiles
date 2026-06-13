64b4f01cfb4a8671396ceb58b455d4b5309a36d7
Murtadha <78985176+MurtadhaInit@users.noreply.github.com>
2026-06-13 12:13:04 +0300

    chore(nixos): removed celluloid (playback issues) and added meld (diff gui tool) and ffmpeg

--- system-setup/nix/hm-modules/linux/packages.nix
+++ system-setup/nix/hm-modules/linux/packages.nix
@@ -45,8 +45,9 @@ in
       tmux
       remmina
       tlrc
-      celluloid
       mpv
+      meld
+      ffmpeg
 
       # # It is sometimes useful to fine-tune packages, for example, by applying
       # # overrides. You can do that directly here, just don't forget the
