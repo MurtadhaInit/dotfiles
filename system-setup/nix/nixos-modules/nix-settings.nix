c4bf90da361043fa2f80bddf25e9c470483e008d
Murtadha <78985176+MurtadhaInit@users.noreply.github.com>
2026-07-17 15:53:48 +0300

    docs(nixos): minor comment changes

--- system-setup/nix/nixos-modules/nix-settings.nix
+++ system-setup/nix/nixos-modules/nix-settings.nix
@@ -7,6 +7,12 @@
     "flakes"
   ];
 
+  # nix.gc = {
+  #   automatic = true;
+  #   dates = "weekly";
+  #   options = "--delete-older-than 7d";
+  # };
+
   # Weekly store deduplication (hardlinks identical files).
   # Complements the generation cleanup done by nh clean (see nh.nix).
   nix.optimise.automatic = true;
